a207e6bdaf830c39a637a78fcf4ea21e7c3a17d9
Edabouba01 <Marikoben111@gmail.com>
2023-10-21 15:10:23 -0300

    partie 1 et 2

--- src/TemperatureSensor.java
+++ src/TemperatureSensor.java
@@ -1,9 +1,24 @@
 import com.fazecast.jSerialComm.*;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import com.fazecast.jSerialComm.SerialPort;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.Properties;
+import org.postgresql.Driver;
 
 public class TemperatureSensor {
 
     public static void main(String[] args) {
+
+        String dbUrl = "jdbc:postgresql://localhost:5432/temphum";
+
+        // Créer un objet Properties pour les propriétés de connexion
+        Properties properties = new Properties();
+        properties.setProperty("user", "postgres");
+        properties.setProperty("password", "Marikoben10");
+
         // Définition du port série
         SerialPort serialPort = SerialPort.getCommPort("COM3"); // Remplacez par le port de votre Arduino
         serialPort.setBaudRate(9600);
@@ -23,25 +38,39 @@ public class TemperatureSensor {
             e.printStackTrace();
         }
 
-        
-
-       // Lecture en continu de données depuis le port série
+        // Lecture en continu de données depuis le port série
         try {
+            Connection connection = DriverManager.getConnection(dbUrl, properties);
+
+            System.out.println("Connecté à la base de données PostgreSQL");
+
             while (true) {
                 byte[] readBuffer = new byte[20];
                 int numRead = serialPort.readBytes(readBuffer, readBuffer.length);
 
                 if (numRead > 0) {
                     String data = new String(readBuffer, 0, numRead);
-                    System.out.print("Données temperature : " + data);
+                    System.out.print("Données humidite : " + data);
+             
 
-                  
-                    byte[] HumiditeData = new byte[20];
-                    int HumiditeRead = serialPort.readBytes(HumiditeData, HumiditeData.length);
+                    // Créez une requête SQL pour insérer les données dans la table "mesuresHum"
+                    String insertQuery = "INSERT INTO mesures(humidite, timestamp) VALUES (?, ?)";
 
-                    if (HumiditeRead > 0) {
-                        String HumiditeValue = new String(HumiditeData, 0, HumiditeRead);
-                        System.out.println("Humidite : " + HumiditeValue + "mv");
+                    try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+                        // Remplacez les ? par les valeurs correspondantes
+                        preparedStatement.setString(1, "Sen0018"); // Remplacez par le nom du capteur
+                        // Remplacez les ? par les valeurs correspondantes
+                        preparedStatement.setString(1, data); // Remplacez par l'humidité mesurée (de type
+                                                              // double)
+                        preparedStatement.setTimestamp(2, new java.sql.Timestamp(new Date().getTime())); // Utilisez la
+                                                                                                         // date
+                                                                                                         // actuelle
+
+                        // Exécutez la requête d'insertion
+                        preparedStatement.executeUpdate();
+
+                    } catch (SQLException e) {
+                        e.printStackTrace();
                     }
                 }
 
