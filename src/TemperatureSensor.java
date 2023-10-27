46cfb50a19c2a7ab7e279aca0d282556365e505e
Edabouba01 <Marikoben111@gmail.com>
2023-10-27 13:54:24 -0300

    fin

--- src/TemperatureSensor.java
+++ src/TemperatureSensor.java
@@ -1,26 +1,49 @@
-import com.fazecast.jSerialComm.*;
+import com.fazecast.jSerialComm.*;/** packages nécessaires à la communication série avec jSerialComm */
+
+/**la gestion de la base de données PostgreSQL */
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import com.fazecast.jSerialComm.SerialPort;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Date;
-import java.util.Properties;
-import org.postgresql.Driver;
+
+import java.util.Date;/**representer la date */
+import java.util.Properties;/**gérer des propriétés sous forme de paires clé-valeur utilisée pour la gestion de configurations */
+import org.postgresql.Driver;/**permet d’accéder à la classe du pilote PostgreSQL, qui implémente l’interface JDBC () */
+
+
+/**
+ * Le pilote JDBC PostgreSQL permet aux programmes Java de se connecter à une
+ * base de données PostgreSQL à l’aide d’une base de données standard code Java
+ * indépendant. pgJDBC est un pilote JDBC open source écrit en Java pur (Type
+ * 4), et communique dans le répertoire Protocole réseau natif PostgreSQL. Pour
+ * cette raison, le conducteur est indépendant de la plate-forme ; Une fois
+ * compilés, les Le pilote peut être utilisé sur n’importe quel système
+ * 
+ * liens :https://jdbc.postgresql.org/
+ */
 
 public class TemperatureSensor {
 
     public static void main(String[] args) {
 
-        String dbUrl = "jdbc:postgresql://localhost:5432/temphum";
+        String dbUrl = "jdbc:postgresql://localhost:5432/temphum";/**
+                                                                   * indiquation de l utilisation du pilote JDBC
+                                                                   * PostgreSQL pour établir une connexion.au port 5432
+                                                                   * avec la base de donnee temphum
+                                                                   */
 
         // Créer un objet Properties pour les propriétés de connexion
+        /**
+         * L utilisation de properties a du s imposer car l objet properties sera
+         * appeller dans getconnection pour revoyer les donner qui lui ont ete affecter
+         */
         Properties properties = new Properties();
         properties.setProperty("user", "postgres");
         properties.setProperty("password", "Marikoben10");
 
         // Définition du port série
-        SerialPort serialPort = SerialPort.getCommPort("COM3"); // Remplacez par le port de votre Arduino
+        SerialPort serialPort = SerialPort.getCommPort("COM3"); // port de lecture Arduino
         serialPort.setBaudRate(9600);
 
         // Ouvrir le port série
@@ -40,35 +63,46 @@ public class TemperatureSensor {
 
         // Lecture en continu de données depuis le port série
         try {
-            Connection connection = DriverManager.getConnection(dbUrl, properties);
+            Connection connection = DriverManager.getConnection(dbUrl, properties);/**
+                                                                                    * Nous utilisons la bibliotheque
+                                                                                    * pilote pour etablir la connection
+                                                                                    * avec la base de donnee
+                                                                                    */
 
             System.out.println("Connecté à la base de données PostgreSQL");
+            /** Lorsque cest connecter ca retourne un message */
 
             while (true) {
-                byte[] readBuffer = new byte[20];
+                byte[] readBuffer = new byte[20];/** stocker les données lues dans le port série */
                 int numRead = serialPort.readBytes(readBuffer, readBuffer.length);
+                /** lire les données depuis le port série avec a longueur maximal desirer */
 
+                /**si longueur est supperieur a 0 alors il ya des donnee */
                 if (numRead > 0) {
-                    String data = new String(readBuffer, 0, numRead);
+                    String data = new String(readBuffer, 0, numRead);/**
+                                                                      * utilisée pour convertir un tableau d'octets en
+                                                                      * une chaîne de caractères en Java
+                                                                      */
                     System.out.print("Données humidite : " + data);
-             
 
-                    // Créez une requête SQL pour insérer les données dans la table "mesuresHum"
+                    // requête SQL pour insérer les données dans la table "mesures"
                     String insertQuery = "INSERT INTO mesures(humidite, timestamp) VALUES (?, ?)";
-
+                    /** ouvre un bloc pour gérer automatiquement */
                     try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
-                        // Remplacez les ? par les valeurs correspondantes
-                        preparedStatement.setString(1, "Sen0018"); // Remplacez par le nom du capteur
-                        // Remplacez les ? par les valeurs correspondantes
-                        preparedStatement.setString(1, data); // Remplacez par l'humidité mesurée (de type
-                                                              // double)
-                        preparedStatement.setTimestamp(2, new java.sql.Timestamp(new Date().getTime())); // Utilisez la
+
+                        preparedStatement.setString(1, "Sen0018"); // le nom du capteur
+
+                        preparedStatement.setString(1, data); /** configure */
+                        preparedStatement.setTimestamp(2, new java.sql.Timestamp(new Date().getTime())); // prendre la
                                                                                                          // date
                                                                                                          // actuelle
 
                         // Exécutez la requête d'insertion
                         preparedStatement.executeUpdate();
 
+                        /**
+                         * gere les exception qui pourrait arrivee lors de la transmission avec postgre
+                         */
                     } catch (SQLException e) {
                         e.printStackTrace();
                     }
@@ -78,7 +112,7 @@ public class TemperatureSensor {
                 System.out.println(); // Ajoutez une nouvelle ligne pour la prochaine série de lectures
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            e.printStackTrace();// gere les exception
         } finally {
             // Fermeture du port série
             serialPort.closePort();
